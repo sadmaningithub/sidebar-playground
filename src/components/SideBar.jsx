71abfa8ee579417a5a2fe67f41e59e395e2ce879
sadmaningithub <sadmanahmed261@gmail.com>
2025-09-22 15:21:26 +0600

    add collapsible sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,38 @@
+import { ArrowLeftToLine, ArrowRightToLine, Contact, House, Mail, ShoppingBag } from "lucide-react";
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 const SideBar = () => {
 
+    const [show, setShow] = useState(true)
+
     const navLinks = [
-        { name: 'Home', path: '/' },
-        { name: 'Products', path: 'products' },
-        { name: 'Contact', path: 'contact' }
+        { name: 'Home', path: '/' , icon: <House size={30}/> },
+        { name: 'Products', path: 'products', icon: <ShoppingBag size={30}/>},
+        { name: 'Contact', path: 'contact', icon: <Mail size={30}/>}
     ]
 
     return (
-        <div className="min-h-screen bg-black text-white text-3xl flex flex-col gap-5 p-5">
+        <div className={`min-h-screen ${show? "" : "min-w-fit" } bg-black text-white text-xl flex flex-col gap-5 p-5`}>
+                <div>
+                    <button onClick={()=> setShow(!show)} className="px-4 py-2 border rounded-lg">
+                        {
+                           show? <ArrowLeftToLine /> : <ArrowRightToLine />
+                        }
+                    </button>
+                </div>
             
                 {
                     navLinks.map(navLink => 
                     <NavLink 
-                    className={({ isActive }) => isActive ? "text-white" : "text-gray-500"} 
+                    key={navLink.name}
+                    className={`${({ isActive }) => isActive ? "text-white" : "text-gray-500"} flex flex-row items-center gap-2 px-4 py-2`} 
                     to={navLink.path}
                     >
-                    {navLink.name}
+                    <span> {navLink.icon} </span>
+                    {
+                        show && <span>{navLink.name}</span>
+                    }
                     </NavLink>)
                 }
             
